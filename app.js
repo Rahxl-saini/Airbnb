ba745f80a8b2ef79ae7e62ee547ae863cf8bcb46
Rahul Saini <rahulsaini2522@gmail.com>
2026-01-20 23:21:55 +0530

    update file

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
+const {listingSchema} = require("./schema.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 main().then(() =>{
@@ -29,6 +30,16 @@ app.get("/", (req, res) =>{
     res.send("I am root");
 });
 
+const validateListing = (req, res, next) =>{
+    let {error} = listingSchema.validate(req.body);
+    console.log(error);
+    if(error){
+        throw new ExpressError(400, error);
+    }else{
+        next();
+    }
+}
+
 // index routes
 app.get("/listings", wrapAsync(async(req, res)=>{
     const allListings = await Listing.find({});
@@ -47,8 +58,7 @@ app.get("/listings/:id", wrapAsync(async(req, res)=>{
     res.render("listings/show.ejs", {listing}); 
 }));
 // Create Route
-app.post("/listings", wrapAsync(async(req, res, next) =>{
-    if(!req.body.listing) throw new ExpressError(400, "Invalid Listing Data");
+app.post("/listings", validateListing, wrapAsync(async(req, res, next) =>{
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
@@ -61,7 +71,7 @@ app.get("/listings/:id/edit", wrapAsync(async(req, res) =>{
     res.render("listings/edit.ejs", {listing});
 }));
 // Update Route
-app.put("/listings/:id", wrapAsync(async(req, res) =>{
+app.put("/listings/:id", validateListing, wrapAsync(async(req, res) =>{
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id, req.body.listing);
     res.redirect(`/listings/${id}`);
